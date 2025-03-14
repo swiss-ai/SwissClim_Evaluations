0a7cd55cf2071a7228f822ce0a1f828d18bc31e5
Francesco Zanetta <zanetta.francesco@gmail.com>
2025-03-14 23:25:48 +0100

    move metrics functions and create wbx metrics classes

--- src/swissclim_evaluations/metrics/probabilistic.py
+++ src/swissclim_evaluations/metrics/probabilistic.py
@@ -1,6 +1,42 @@
 import numpy as np
 import xarray as xr
 
+def _crps_e1(obs, fct):
+    M: int = fct.shape[-1]
+    e_1 = np.sum(np.abs(obs[..., None] - fct), axis=-1) / M
+    return e_1
+
+def crps_e1(obs, fct, name_prefix: str = "CRPS", ensemble_dim="ensemble"):
+    """Compute the CRPS for ensemble forecasts."""
+    return xr.apply_ufunc(
+        _crps_e1,
+        obs,
+        fct,
+        input_core_dims=[[], [ensemble_dim]],
+        output_core_dims=[[]],
+        dask="parallelized",
+    )
+
+def _crps_e2(fct):
+    M: int = fct.shape[-1]
+    e_2 = np.sum(
+        np.abs(fct[..., None] - fct[..., None, :]),
+        axis=(-1, -2),
+    ) / (M * (M - 1))
+    return e_2
+
+def crps_e2(fct, name_prefix: str = "CRPS", ensemble_dim="ensemble"):
+    """Compute the CRPS for ensemble forecasts."""
+    return xr.apply_ufunc(
+        _crps_e2,
+        fct,
+        input_core_dims=[[ensemble_dim]],
+        output_core_dims=[[]],
+        dask="parallelized",
+    )
+
+
+
 
 
 def _crps_ensemble_fair(obs, fct):
@@ -12,13 +48,13 @@ def _crps_ensemble_fair(obs, fct):
     ) / (M * (M - 1))
     return e_1 - 0.5 * e_2
 
-def crps_ensemble(obs, fct, name_prefix: str = "CRPS"):
+def crps_ensemble(obs, fct, name_prefix: str = "CRPS", ensemble_dim="ensemble"):
     """Compute the CRPS for ensemble forecasts."""
     res = xr.apply_ufunc(
         _crps_ensemble_fair,
         obs,
         fct,
-        input_core_dims=[[], ["ensemble"]],
+        input_core_dims=[[], [ensemble_dim]],
         output_core_dims=[[]],
         dask="parallelized",
     )
@@ -28,17 +64,17 @@ def crps_ensemble(obs, fct, name_prefix: str = "CRPS"):
 def _pit(obs, fct):
     return np.mean(fct < obs[...,None], axis=-1)
 
-def probability_integral_transform(obs, fct, name_prefix: str = "PIT"):
+def probability_integral_transform(obs, fct, ensemble_dim="ensemble", name_prefix: str = "PIT"):
     """Compute the probability integral transform."""
     res = xr.apply_ufunc(
         _pit,
         obs,
         fct,
-        input_core_dims=[[], ["ensemble"]],
+        input_core_dims=[[], [ensemble_dim]],
         output_core_dims=[[]],
         dask="parallelized",
     )
-    return _add_metric_prefix(res, name_prefix)
+    return _add_metric_prefix(res, name_prefix) if name_prefix else res
 
 
 def _ens_mean_se(obs, fct):
@@ -69,9 +105,8 @@ def ensemble_std(fct, name_prefix: str = "EnsembleSTD"):
         output_core_dims=[[]],
         dask="parallelized",
     )
-    return _add_metric_prefix(res, name_prefix)
+    return _add_metric_prefix(res, name_prefix) if name_prefix else res
 
 
 def _add_metric_prefix(ds: xr.Dataset, prefix: str):
     return ds.rename({var: f"{prefix}.{var}" for var in ds.data_vars})
-
