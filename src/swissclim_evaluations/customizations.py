459971cd1c593b6184a3d8fefb6561f7dd3e19ff
Fanny Lehmann <58693544+lehmannfa@users.noreply.github.com>
2025-11-26 17:53:35 +0100

    remove debug message

--- src/swissclim_evaluations/customizations.py
+++ src/swissclim_evaluations/customizations.py
@@ -3,8 +3,6 @@ import xarray as xr
 def modify_ds(ds: xr.Dataset, path: str):
     ''' User-defined customization to the dataset based on the full path to the zarr file '''
 
-    print("inside custom", path)
-    print(ds)
     if path == "/capstor/store/cscs/swissai/a122/hydrological_data/IFS_ensmean_2020_totalprecipitation6hr.zarr":
         ds = ds.sel(prediction_timedelta = ds.prediction_timedelta.values[1:]) # the first step is 0 hour and full of NaN
 
